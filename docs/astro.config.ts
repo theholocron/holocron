5a3fa62f4c126168bf625e48d5694a2e866cd70b
Newton <5769156+iamnewton@users.noreply.github.com>
2026-08-01 17:07:21 -0700

    fix(docs): pass starlight and docsTheme to defineConfig
    Signed-off-by: Newton <5769156+iamnewton@users.noreply.github.com>

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,8 +1,12 @@
+import starlight from "@astrojs/starlight";
+import { docsTheme } from "@theholocron/docs-theme";
 import { defineConfig } from "@theholocron/astro-config";
 import holocronConfig from "@theholocron/holocron-docs";
 
 export default defineConfig({
 	docs: holocronConfig,
 	importMetaUrl: import.meta.url,
+	starlight,
+	docsTheme,
 	sidebarLabel: "Reference",
 });
